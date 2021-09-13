7a4669fb1ebb42c35c7b1539efcad4727ed66b11
catanielena <catani.elena1@gmail.com>
2021-09-13 11:05:24 +0200

    Add: icons

---
+++ js/main.js
@@ -0,0 +1,124 @@
+//icons
+const icons = [
+	{
+	  name: 'apple-alt',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'ice-cream',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'fish',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'lemon',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'hamburger',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'pizza-slice',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'beer',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'glass-whiskey',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'wine-bottle',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'cocktail',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'coffee',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'glass-martini',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'dragon',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'kiwi-bird',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'frog',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'hippo',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'otter',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'horse',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+  ];
+
+const iconsWrapper = document.getElementById("icons");
+icons.forEach(
+    (e) => {
+        iconsWrapper.innerHTML += `
+        <div class="icons-wrapper__card">
+           <i class="card__icon ${e.family} ${e.prefix + e.name}"></i>
+           <div class="card__label">
+                ${e.name}
+           </div>
+        </div>`
+    }
+);
